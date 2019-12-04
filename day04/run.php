17af28470ada38be17f936d2c643750448100c42
Ruud <r.bijnen@gmail.com>
2019-12-04 23:46:00 +0100

    Day 4: part 1, simple brute force solution

---
+++ day04/run.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+$args = array_slice($_SERVER['argv'], 1);
+$inFile = $args[1] ?? 'input.txt';
+
+[$min, $max] = array_map('intval', explode('-', trim(file_get_contents($inFile))));
+
+var_dump(compact('min', 'max'));
+
+$valid = [];
+
+for ($current = $min; $current <= $max; $current++) {
+    $digits = array_map('intval', str_split((string)$current));
+    $hasDouble = false;
+    foreach ($digits as $i => $digit) {
+        if (($prev = $digits[$i - 1] ?? null) === null) {
+            continue;
+        }
+        if ($digit < $prev) {
+            continue 2;
+        }
+        if ($digit === $prev) {
+            $hasDouble = true;
+        }
+    }
+    if ($hasDouble) {
+        $valid[] = $current;
+        var_dump($current);
+    }
+}
+
+echo 'Result part1: ', count($valid), PHP_EOL;
