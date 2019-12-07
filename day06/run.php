43ca279e7fbb44d5ecc7cd84e242c643d883030f
Ruud <r.bijnen@gmail.com>
2019-12-07 01:06:11 +0100

    Day 6: part 2

--- day06/run.php
+++ day06/run.php
@@ -20,6 +20,18 @@ class O
     {
         return $this->parent ? $this->parent->getOrbitCount() + 1 : 0;
     }
+
+    /** @return O[] */
+    public function getPath(): array
+    {
+        $path = [];
+        if ($this->parent !== null) {
+            $path = $this->parent->getPath();
+        }
+        $path[$this->name] = $this;
+
+        return $path;
+    }
 }
 
 /** @return O[] */
@@ -50,7 +62,8 @@ function totalOrbits(array $objects): int
     return array_sum(array_map(fn(O $o) => $o->getOrbitCount(), $objects));
 }
 
-$in = <<<IN
+
+$example1 = <<<IN
 COM)B
 B)C
 C)D
@@ -64,13 +77,55 @@ J)K
 K)L
 IN;
 
-assert(totalOrbits(readObject($in)) === 42);
+$example1Objects = readObject($example1);
+assert(totalOrbits($example1Objects) === 42);
 
 $args = array_slice($_SERVER['argv'], 1);
 $inFile = $args[1] ?? 'input.txt';
 $in = file_get_contents($inFile);
 
+ini_set('xdebug.max_nesting_level', '512');
 $objects = readObject($in);
+unset($in);
 //$com = $objects['COM'];
 
 echo 'Result part1: ', totalOrbits($objects), PHP_EOL;
+
+/** @param O[] $objects */
+function minTransfers(array $objects): int
+{
+    $youPath = array_keys($objects['YOU']->getPath());
+    $sanPath = array_keys($objects['SAN']->getPath());
+    $sameCount = 0;
+    foreach ($youPath as $i => $objectName) {
+        if (($sanPath[$i] ?? null) !== $objectName) {
+            break;
+        }
+        $sameCount++;
+    }
+
+    return count($youPath) - $sameCount + count($sanPath) - $sameCount - 2;
+}
+
+$example2 = <<<IN
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN
+IN;
+
+$example2Objects = readObject($example2);
+
+assert(minTransfers($example2Objects) === 4);
+
+echo 'Result part2: ', minTransfers($objects), PHP_EOL;
+
