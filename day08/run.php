d38652e1df42a70582f4b40ab6805d246121771e
Ruud <r.bijnen@gmail.com>
2019-12-10 22:22:07 +0100

    Day 8: part 2

--- day08/run.php
+++ day08/run.php
@@ -8,16 +8,18 @@ $data = trim(file_get_contents($inFile));
 
 function image(string $data, int $width, int $height): array
 {
-    $layers = array_map(function (string $layer) use ($height) {
+    $layers = array_map(function (string $layer) use ($width) {
         return array_map(function (string $row) {
             return array_map('intval', str_split($row));
-        }, str_split($layer, $height));
+        }, str_split($layer, $width));
     }, str_split($data, $width * $height));
 
     return $layers;
 }
 
-$image = image($data, 25, 6);
+$width = 25;
+$height = 6;
+$image = image($data, $width, $height);
 
 $layerStats = array_map(function (array $layer) {
     $pixels = array_merge(...$layer);
@@ -28,3 +30,22 @@ $layerZeroCount = array_column($layerStats, 0);
 $layerWithLeastZeros = array_search(min($layerZeroCount), $layerZeroCount, true);
 
 echo 'Result part1: ', ($layerStats[$layerWithLeastZeros][1] * $layerStats[$layerWithLeastZeros][2]), PHP_EOL;
+
+$lastLayer = array_key_last($image);
+$mergedImage = array_fill(0, $height, array_fill(0, $width, '?'));
+for ($y = 0; $y < $height; $y++) {
+    for ($x = 0; $x < $width; $x++) {
+        foreach ($image as $layer => $rows) {
+            $color = $rows[$y][$x];
+            if ($color !== 2) {
+                $mergedImage[$y][$x] = $color ? '#' : ' ';
+                break;
+            }
+        }
+    }
+}
+
+echo 'Result part2:', PHP_EOL;
+foreach ($mergedImage as $row) {
+    echo implode($row), PHP_EOL;
+}
